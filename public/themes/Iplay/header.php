ccbf3882625e124c54c658103b8863a7b9a5b662
krisKurken <kristian.liljander@live.se>
2018-03-24 11:10:23 +0100

    Init the Iplay theme

---
+++ public/themes/Iplay/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+
+  <?php wp_head(); ?>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-inverse navbar-dark mb-3" style="background: #001a52">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <a class="navbar-brand" href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <?php foreach (get_pages() as $page): ?>
+          <li class="nav-item <?php if (is_page($page)) { echo 'active'; } ?>">
+            <a class="nav-link" href="<?php echo get_permalink($page); ?>">
+              <?php echo $page->post_title; ?>
+            </a>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
