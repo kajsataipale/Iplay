a2c4b6ce1fd50b40d62f26a6be4927ea25527af2
“Kajsa <31646322+kajsataipale@users.noreply.github.com>
2018-03-27 11:16:03 +0200

    added the logo at the login page

--- public/themes/Iplay/includes/plugins/plate.php
+++ public/themes/Iplay/includes/plugins/plate.php
@@ -71,7 +71,7 @@ add_theme_support('plate-disable-tabs', ['help', 'screen-options']);
 add_theme_support('plate-permalink', '/%postname%/');
 
 // Set custom login logo.
-add_theme_support('plate-login-logo', asset('assets/images/logo.png'));
+add_theme_support('plate-login-logo', asset('screenshot.png'));
 
 // Set custom footer text.
 add_theme_support('plate-footer-text', 'Customize the future of sports!');
